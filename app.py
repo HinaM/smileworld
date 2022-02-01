404b85bdfa8c1a45e1d11e8f6af73a7d0093b903
HinaM <55233191+HinaM@users.noreply.github.com>
2022-02-01 23:55:01 +0800

    識別

--- app.py
+++ app.py
@@ -44,8 +44,11 @@ def callback():
 def handle_message(event):
     message=event.message.text
     message=message.encode('utf-8')
-    user_id = event.source.user_id
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.source.user_id))
+    if event.message.text=="識別碼":
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.source.user_id))
+    else:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+    
 
 #主程式
 import os
