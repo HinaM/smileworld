c79028d6391363af14c9748ea04b32978e7bda94
HinaM <55233191+HinaM@users.noreply.github.com>
2022-02-01 21:48:09 +0800

    4

--- app.py
+++ app.py
@@ -40,7 +40,10 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+    if message=="微笑世界":
+        line_bot_api.reply_message(event.reply_token,"吸很多")
+    else:
+        line_bot_api.reply_message(event.reply_token,message)
 
 #主程式
 import os
