de7307c24ed4c32e3894efee0f2a44e341dcf35c
HinaM <55233191+HinaM@users.noreply.github.com>
2022-02-12 00:22:48 +0800

    de

--- app.py
+++ app.py
@@ -137,6 +137,7 @@ def handle_message(event):
                 ]
             )
         )
+        line_bot_api.reply_message(event.reply_token,carousel_template_message)
     elif event.message.text=="開始遊戲":
         userid_list=worksheet.col_values(1)
         if event.source.user_id in userid_list:
