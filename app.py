fcebdf71b2a1e4727dc9a4c9828617aa372fc850
Fjuim110 <fjuim110@gmail.com>
2022-04-01 01:20:56 +0800

    #18

--- app.py
+++ app.py
@@ -760,6 +760,55 @@ def handle_message(event):
         else:
             line_bot_api.reply_message(event.reply_token,TextSendMessage(text="輸入錯誤"))
 
+    #17答案
+    elif event.message.text=="2e^(2x)":
+        userid_list=worksheet.col_values(1)
+        if event.source.user_id in userid_list:
+            for i in range(len(userid_list)):
+                if userid_list[i]==event.source.user_id:
+                    j=i+1
+            list=[]
+            list.append('D'+str(j))
+            list.append('U'+str(j))
+            list.append('V'+str(j))
+            #ID已寫入、日向視角、Q2=1
+            if worksheet.acell(list[0]).value=="1" and worksheet.acell(list[1]).value=="1":
+                worksheet.update(list[1],int(2))
+                worksheet.update(list[2],int(1))
+                list_talk=[]
+                list_talk.append(TextSendMessage(text="#18 下列何者為會計恆等式？"+"\n"+"（Ａ）資產 = 負債 + 權益"+"\n"+"（Ｂ）資產 – 權益 = 存貨"+"\n"+"（Ｃ）資產 = 權益"+"\n"+"（Ｄ）負債 = 權益 – 資產"))
+                buttons_template_message = TemplateSendMessage(
+                    alt_text='#18',
+                    template=ButtonsTemplate(
+                        title='#18',
+                        text='請選出正確答案',
+                        actions=[
+                            MessageAction(
+                                label='A',
+                                text='資產 = 負債 + 權益'
+                            ),
+                            MessageAction(
+                                label='B',
+                                text='資產 – 權益 = 存貨'
+                            ),
+                            MessageAction(
+                                label='C',
+                                text='資產 = 權益'
+                            ),
+                            MessageAction(
+                                label='D',
+                                text='負債 = 權益 – 資產'
+                            )
+                        ]
+                    )
+                )
+                list_talk.append(buttons_template_message)
+                line_bot_api.reply_message(event.reply_token,list_talk)
+            else:
+                line_bot_api.reply_message(event.reply_token,TextSendMessage(text="輸入錯誤"))
+        else:
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text="輸入錯誤"))
+
     elif event.message.text=="遊戲規則":
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text="本遊戲是採用回答問題的遊玩方式進行闖關！！"+"\n"+"玩家回答出遊戲內關卡的問題，透過回答問題一步步解鎖劇情✨"+"\n"+"若是問題回答不出來時可以參考下面網站裡的解題技巧喔٩( 'ω' )و "+"\n"+"玩家從個人檔案中觀看目前選擇視角、已解鎖物件，想重新體驗遊戲或選擇不同視角可以輸入「重置遊戲」喔✨"+"\n\n"+"最後祝各位玩家遊玩愉快🥳"))
     elif event.message.text=="人物介紹":
