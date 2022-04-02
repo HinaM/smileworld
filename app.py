d3c301236d0818da2e502c2a30a065b316fb3dd8
Fjuim110 <fjuim110@gmail.com>
2022-04-02 23:05:44 +0800

    #59

--- app.py
+++ app.py
@@ -2755,6 +2755,50 @@ def handle_message(event):
         else:
             line_bot_api.reply_message(event.reply_token,TextSendMessage(text="輸入錯誤"))
 
+    #59答案
+    elif event.message.text=="舊醫學大樓一樓右側":
+        userid_list=worksheet.col_values(1)
+        if event.source.user_id in userid_list:
+            for i in range(len(userid_list)):
+                if userid_list[i]==event.source.user_id:
+                    j=i+1
+            list=[]
+            list.append('D'+str(j))
+            list.append('E'+str(j))
+            #ID已寫入、日向視角、Q2=1
+            if worksheet.acell(list[0]).value=="1" and worksheet.acell(list[1]).value=="58":
+                worksheet.update(list[1],int(59))
+                list_talk=[]
+                list_talk.append(TextSendMessage(text="專題發表的時刻終於到了！五人組對於這次的專題內容十分有信心。但信心歸信心，發表時刻終究還是有所考驗的。趕緊動用所學，度過專題大魔王吧！"+"\n"+"日翔已經決定好結束這次專題就要向曉光表達心意了，可不能在這個時候漏氣呀。"))
+                list_talk.append(TextSendMessage(text="#60 CPU排班演算法中，效能最佳的是？"+"\n"+"（Ａ）FIFO法則"+"\n"+"（Ｂ）SJF法則"+"\n"+"（Ｃ）RR法則"))
+                buttons_template_message = TemplateSendMessage(
+                    alt_text='#60',
+                    template=ButtonsTemplate(
+                        title='#60',
+                        text='請選出正確答案',
+                        actions=[
+                            MessageAction(
+                                label='A',
+                                text="FIFO法則"
+                            ),
+                            MessageAction(
+                                label='B',
+                                text="SJF法則"
+                            ),
+                            MessageAction(
+                                label='C',
+                                text="RR法則"
+                            )
+                        ]
+                    )
+                )
+                list_talk.append(buttons_template_message)
+                line_bot_api.reply_message(event.reply_token,list_talk)
+            else:
+                line_bot_api.reply_message(event.reply_token,TextSendMessage(text="輸入錯誤"))
+        else:
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text="輸入錯誤"))
+
     elif event.message.text=="遊戲規則":
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text="本遊戲是採用回答問題的遊玩方式進行闖關！！"+"\n"+"玩家回答出遊戲內關卡的問題，透過回答問題一步步解鎖劇情✨"+"\n"+"若是問題回答不出來時可以參考下面網站裡的解題技巧喔٩( 'ω' )و "+"\n"+"玩家從個人檔案中觀看目前選擇視角、已解鎖物件，想重新體驗遊戲或選擇不同視角可以輸入「重置遊戲」喔✨"+"\n\n"+"最後祝各位玩家遊玩愉快🥳"))
     elif event.message.text=="人物介紹":
