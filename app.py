964350469cd93c9d32d747831c3af3b3a678e4b2
Fjuim110 <fjuim110@gmail.com>
2022-04-04 01:38:32 +0800

    #76

--- app.py
+++ app.py
@@ -3427,6 +3427,50 @@ def handle_message(event):
         else:
             line_bot_api.reply_message(event.reply_token,TextSendMessage(text="輸入錯誤"))
 
+    #76答案
+    elif event.message.text=="GNP的全名為國民生產毛額":
+        userid_list=worksheet.col_values(1)
+        if event.source.user_id in userid_list:
+            for i in range(len(userid_list)):
+                if userid_list[i]==event.source.user_id:
+                    j=i+1
+            list=[]
+            list.append('D'+str(j))
+            list.append('E'+str(j))
+            #ID已寫入、日向視角、Q2=1
+            if worksheet.acell(list[0]).value=="1" and worksheet.acell(list[1]).value=="75":
+                worksheet.update(list[1],int(76))
+                list_talk=[]
+                list_talk.append(TextSendMessage(text="很快的季節到了春天，除了下學期來臨以外，更是碩士班放榜的季節！為了關心曉光跟宇桓的錄取狀況，五人組決定來個相聚歡。因為日翔等人畢業了之後就很難再隨時回到學校，於是一行人在討論之中，本來是打算吃學餐的……"))
+                list_talk.append(TextSendMessage(text="#77 下列哪個不是學餐？"+"\n"+"（Ａ）理園"+"\n"+"（Ｂ）法園"+"\n"+"（Ｃ）仁園"))
+                buttons_template_message = TemplateSendMessage(
+                    alt_text='#77',
+                    template=ButtonsTemplate(
+                        title='#77',
+                        text='請選出正確答案',
+                        actions=[
+                            MessageAction(
+                                label='A',
+                                text="理園"
+                            ),
+                            MessageAction(
+                                label='B',
+                                text="法園"
+                            ),
+                            MessageAction(
+                                label='C',
+                                text="仁園"
+                            )
+                        ]
+                    )
+                )
+                list_talk.append(buttons_template_message)
+                line_bot_api.reply_message(event.reply_token,list_talk)
+            else:
+                line_bot_api.reply_message(event.reply_token,TextSendMessage(text="輸入錯誤"))
+        else:
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text="輸入錯誤"))
+
     elif event.message.text=="遊戲規則":
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text="本遊戲是採用回答問題的遊玩方式進行闖關！！"+"\n"+"玩家回答出遊戲內關卡的問題，透過回答問題一步步解鎖劇情✨"+"\n"+"若是問題回答不出來時可以參考下面網站裡的解題技巧喔٩( 'ω' )و "+"\n"+"玩家從個人檔案中觀看目前選擇視角、已解鎖物件，想重新體驗遊戲或選擇不同視角可以輸入「重置遊戲」喔✨"+"\n\n"+"最後祝各位玩家遊玩愉快🥳"))
     elif event.message.text=="人物介紹":
