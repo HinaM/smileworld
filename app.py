8915c5766a9137bfedf06c3cb7c8f552c5ee8989
Fjuim110 <fjuim110@gmail.com>
2022-04-04 01:46:34 +0800

    #77

--- app.py
+++ app.py
@@ -3471,6 +3471,53 @@ def handle_message(event):
         else:
             line_bot_api.reply_message(event.reply_token,TextSendMessage(text="輸入錯誤"))
 
+    #77答案
+    elif event.message.text=="法園":
+        userid_list=worksheet.col_values(1)
+        if event.source.user_id in userid_list:
+            for i in range(len(userid_list)):
+                if userid_list[i]==event.source.user_id:
+                    j=i+1
+            list=[]
+            list.append('D'+str(j))
+            list.append('E'+str(j))
+            #ID已寫入、日向視角、Q2=1
+            if worksheet.acell(list[0]).value=="1" and worksheet.acell(list[1]).value=="76":
+                worksheet.update(list[1],int(77))
+                list_talk=[]
+                list_talk.append(TextSendMessage(text="#78 輔大「五」學餐為？"+"\n"+"（Ａ）輔園、文園、心園、仁園、理園"+"\n"+"（Ｂ）一園、兩園、三園、四園、五園"+"\n"+"（Ｃ）動物園、植物園、伊甸園、公園、球好園"+"\n"+"（Ｄ）日園、美園、歐園、澳園、港園"))
+                buttons_template_message = TemplateSendMessage(
+                    alt_text='#77',
+                    template=ButtonsTemplate(
+                        title='#77',
+                        text='請選出正確答案',
+                        actions=[
+                            MessageAction(
+                                label='A',
+                                text="輔園、文園、心園、仁園、理園"
+                            ),
+                            MessageAction(
+                                label='B',
+                                text="一園、兩園、三園、四園、五園"
+                            ),
+                            MessageAction(
+                                label='C',
+                                text="動物園、植物園、伊甸園、公園、球好園"
+                            ),
+                            MessageAction(
+                                label='D',
+                                text="日園、美園、歐園、澳園、港園"
+                            )
+                        ]
+                    )
+                )
+                list_talk.append(buttons_template_message)
+                line_bot_api.reply_message(event.reply_token,list_talk)
+            else:
+                line_bot_api.reply_message(event.reply_token,TextSendMessage(text="輸入錯誤"))
+        else:
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text="輸入錯誤"))
+
     elif event.message.text=="遊戲規則":
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text="本遊戲是採用回答問題的遊玩方式進行闖關！！"+"\n"+"玩家回答出遊戲內關卡的問題，透過回答問題一步步解鎖劇情✨"+"\n"+"若是問題回答不出來時可以參考下面網站裡的解題技巧喔٩( 'ω' )و "+"\n"+"玩家從個人檔案中觀看目前選擇視角、已解鎖物件，想重新體驗遊戲或選擇不同視角可以輸入「重置遊戲」喔✨"+"\n\n"+"最後祝各位玩家遊玩愉快🥳"))
     elif event.message.text=="人物介紹":
